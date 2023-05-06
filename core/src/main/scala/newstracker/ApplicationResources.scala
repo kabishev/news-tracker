65a7d7555ca76a7f45beeee7b551f40b6184cb9d
Denis Kabishev <kabishev@gmail.com>
2023-05-06 07:17:36 +0200

    refactoring

--- core/src/main/scala/newstracker/ApplicationResources.scala
+++ core/src/main/scala/newstracker/ApplicationResources.scala
@@ -22,25 +22,16 @@ object ApplicationResources {
         .fromConnectionString[F](config.connectionUri)
         .evalMap(_.getDatabase(config.name))
 
-    def makeCreateArticleConsumer(config: KafkaConfig): Resource[F, KafkaConsumer[F, Unit, kafka.createArticle.Event]] =
-      kafka.createArticle.makeConsumer[F](config)
-
-    def makeCreatedArticleProducer(config: KafkaConfig): Resource[F, Producer[F, Unit, kafka.createdArticle.Event]] =
-      kafka.createdArticle.makeProducer[F](config)
-
-    def makeCreatedArticleConsumer(config: KafkaConfig): Resource[F, KafkaConsumer[F, Unit, kafka.createdArticle.Event]] =
-      kafka.createdArticle.makeConsumer[F](config)
-
     for {
-      db              <- mongoDb(config.mongo)
-      createArticleC  <- makeCreateArticleConsumer(config.kafka)
-      createdArticleP <- makeCreatedArticleProducer(config.kafka)
-      createdArticleC <- makeCreatedArticleConsumer(config.kafka)
+      db                 <- mongoDb(config.mongo)
+      createArticleCons  <- kafka.createArticle.makeConsumer[F](config.kafka)
+      createdArticleProd <- kafka.createdArticle.makeProducer[F](config.kafka)
+      createdArticleCons <- kafka.createdArticle.makeConsumer[F](config.kafka)
     } yield new ApplicationResources[F] {
       override val mongo: MongoDatabase[F]                                                    = db
-      override val createArticleConsumer: KafkaConsumer[F, Unit, kafka.createArticle.Event]   = createArticleC
-      override val createdArticleProducer: Producer[F, Unit, kafka.createdArticle.Event]      = createdArticleP
-      override val createdArticleConsumer: KafkaConsumer[F, Unit, kafka.createdArticle.Event] = createdArticleC
+      override val createArticleConsumer: KafkaConsumer[F, Unit, kafka.createArticle.Event]   = createArticleCons
+      override val createdArticleProducer: Producer[F, Unit, kafka.createdArticle.Event]      = createdArticleProd
+      override val createdArticleConsumer: KafkaConsumer[F, Unit, kafka.createdArticle.Event] = createdArticleCons
     }
   }
 }
