0944c38de3fc5a43341009f7f65b3599602ab440
Denis Kabishev <kabishev@gmail.com>
2023-05-18 22:50:09 +0200

    Remove duration from TaskCompletedEvent

--- core/src/main/scala/newstracker/ws/WsEvent.scala
+++ core/src/main/scala/newstracker/ws/WsEvent.scala
@@ -8,10 +8,10 @@ object WsEvent {
   final case class ArticleCreated(articleId: String)                      extends WsEvent
   final case class ArticleTranslated(articleId: String, language: String) extends WsEvent
 
-  final case class ServiceOnline(id: String, serviceName: String)                                      extends WsEvent
-  final case class ServiceOffline(id: String, serviceName: String)                                     extends WsEvent
-  final case class ServiceError(id: String, serviceName: String, error: String)                        extends WsEvent
-  final case class TaskCompleted(id: String, serviceName: String, description: String, duration: Long) extends WsEvent
+  final case class ServiceOnline(id: String, serviceName: String)                      extends WsEvent
+  final case class ServiceOffline(id: String, serviceName: String)                     extends WsEvent
+  final case class ServiceError(id: String, serviceName: String, error: String)        extends WsEvent
+  final case class TaskCompleted(id: String, serviceName: String, description: String) extends WsEvent
 
   object ArticleCreated {
     def from(event: CreatedArticleEvent): ArticleCreated = ArticleCreated(event.id)
